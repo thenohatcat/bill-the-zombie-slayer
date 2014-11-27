885822ca8569c5be2f04f78f9cc2ee09ff028daf
thenohatcat <jakobia_2@hotmail.com>
2014-11-27 17:09:24 +0100

    Added basic components 0.1a

--- Bitchcraft/Game.cpp
+++ Bitchcraft/Game.cpp
@@ -1,10 +1,12 @@
-#if !defined(V0_1r) && !defined(V0_1d)
-#error Game.cpp: Incorrect Version, required: 0.1
+#if !defined(BITCHCRAFT_V0_1r) && !defined(BITCHCRAFT_V0_1d)
+#error Bitchcraft: Game.h: Incorrect Version, required: 0.1
 #endif
 
 #include "Game.h"
 
+#include <iostream>
+
 Bitchcraft::Game::Game()
 {
-
+	std::cout << "Test" << std::endl;
 }
--- Bitchcraft/Game.h
+++ Bitchcraft/Game.h
@@ -3,8 +3,8 @@
 #ifndef BITCHCRAFT_GAME_inc
 #define BITCHCRAFT_GAME_inc
 
-#if !defined(V0_1r) && !defined(V0_1d)
-#error Game.h: Incorrect Version, required: 0.1
+#if !defined(BITCHCRAFT_V0_1r) && !defined(BITCHCRAFT_V0_1d)
+#error Bitchcraft: Game.h: Incorrect Version, required: 0.1
 #endif
 
 #define DllImport   __declspec( dllimport )
